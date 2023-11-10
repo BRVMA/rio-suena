718d7ff6461a3aa124dfbe7d4efa8cbb890ee8c3
Matías Andrade <matiac@pm.me>
2023-11-10 19:55:44 -0300

    Nosotros: contenido

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,10 +5,26 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+            colors: {
+                crema: '#efefef',
+                verde: '#b0bcaf',
+                celeste: '#b9dbe5',
+                gris: '#808080',
+                negro: '#1a1a1a',
+            },
             fontFamily: {
                 'sans': ['Manrope', ...defaultTheme.fontFamily.sans],
                 'zighead': 'Zighead',
-            }
+            },
+            keyframes: {
+                'fade-in': {
+                    '0%': { opacity: '0%' },
+                    '100%': { opacity: '100%' },
+                }
+            },
+            animation: {
+                'fade-in': 'fade-in 150ms ease',
+            },
         },
 	},
 	plugins: [],
