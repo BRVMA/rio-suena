f022bdaf76ab6ad78add20beaae6966fddb82e18
Matías Andrade <matiac@pm.me>
2024-06-19 19:40:51 -0400

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,24 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-
 const env = loadEnv('', process.cwd(), 'STORYBLOK');
-
 import svelte from '@astrojs/svelte';
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        post: 'storyblok/post',
-      },
-      apiOptions: {
-        region: 'us',
-      },
-    }),
-    svelte(),
-  ],
+  integrations: [tailwind(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      post: 'storyblok/post'
+    },
+    apiOptions: {
+      region: 'us'
+    }
+  }), svelte()],
   image: {
-    domains: ['storyblok.com'],
+    domains: ['storyblok.com']
   },
+  output: "server",
+  adapter: netlify()
 });
